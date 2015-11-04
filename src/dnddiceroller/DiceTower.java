dab252ce1a418ae23d549c0c5b998bcf1fed0eb0
Zammy95 <oswaldj@my.wccc.edu>
2015-11-04 01:31:04 -0500

    Commit
    Assignment 7 Dice Tower

--- src/dnddiceroller/DiceTower.java
+++ src/dnddiceroller/DiceTower.java
@@ -6,23 +6,43 @@ import java.util.List;
 /**
  * Dice Tower.
  * A Dice Tower is a tool used by serious gamers use to roll many dice at once.
- * It looks like this https://www.miniaturescenery.com/Images/PortableDiceTowerLarge.jpg
- * An instance of a dice tower is defined by the number panels it contains to help
- * provide a more regular distribution of die values. The die bounce from panel to
- * panel until they exit the dice tower at the bottom tray.
+ * It looks like this:
+ * https://www.miniaturescenery.com/Images/PortableDiceTowerLarge.jpg
+ * An instance of a dice tower is defined by the number panels it
+ * contains to help provide a more regular distribution of die values.
+ * The die bounce from panel to panel until they
+ * exit the dice tower at the bottom tray.
  * A dice tower will accept a collection of dice and reports their results when
  * they reach the tray at the bottom
  * @author Paul Scarrone
  */
 public class DiceTower {
-  final int PANEL_COUNT = 3;
-  List<Die> dice;
+  private final int PANEL_COUNT = 3; //number of panels in the dice tower
+  private int trayValue;  //combined value of rolled dice
+  private List<Die> dice;
 
   public DiceTower() {
 	this.dice = new ArrayList();
+        this.trayValue = 0;
   }
   
   public DiceTower(List dice) {
 	this.dice = dice;
+        this.trayValue = 0;
+  }
+  
+  public int getTrayValue() {
+    return this.trayValue;
+  }
+  
+  //rolls each dice three times using the
+  //roll method from the Die class
+  public void DropDice(){
+    for (int currentDie = 0; currentDie < dice.size(); currentDie++) {      
+      for (int p = 0; p < PANEL_COUNT; p++) {
+        this.dice.get(currentDie).roll();
+      }
+      trayValue += this.dice.get(currentDie).getResult();
+    }
   }
 }
--- src/dnddiceroller/Die.java
+++ src/dnddiceroller/Die.java
@@ -1,5 +1,7 @@
 package dnddiceroller;
 
+import java.util.Random;
+
 /**
  * A Die is a many sided object that when rolled provides a random value from
  * 1 through the number of sides on the object. Some dice are 6 sided and have
@@ -8,5 +10,35 @@ package dnddiceroller;
  * @author Paul Scarrone
  */
 public class Die {
+  
+  //requried fields
+  private int dieSize;
+  private int result;
+
+  /**
+   *Die Constructor
+   * 
+   * @param dieSize
+   */
+    public Die(int dieSize){
+    this.dieSize = dieSize;
+    this.result = 0;
+  }
+  
+  //acquries the possible die results
+  public int getDieSize(){
+    return this.dieSize;
+  }
+  
+  //returns the die's result
+  public int getResult(){
+    return this.result;
+  }
+  
+  //roll method used to roll a single die
+  public void roll() {
+    Random randomRoll = new Random();
+    this.result = randomRoll.nextInt(this.dieSize) + 1;
+  }
   
 }
